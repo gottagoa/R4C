80b2ae640cb1c5543086e1f9d46106176f07d28e
gottagoa <136464755+gottagoa@users.noreply.github.com>
2023-09-30 21:55:29 -0700

    delete prints

--- robots/signals.py
+++ robots/signals.py
@@ -15,15 +15,12 @@ from robots.models import Robot
 
 @receiver(post_save, sender=Robot)
 def email_notification(sender, instance, created, **kwargs):
-    print("Signal working!")
     if created: # Если был создан новый robots
-        print(instance.serial)
         orders = Order.objects.filter(
             is_abcent=False, 
             is_notificated=False
             ).filter(robot_serial=instance.serial)
         if orders:
-            print("Заказы найдены")
             email_subject = "Новое поступление роботов"
             message = render_to_string("email_notification.html", {"robot": instance})
             email_list = list(orders.values_list("customer__email", flat=True))
